86847b72c299c7fc54ea2af55cb50f56965c956c
Timur <prytotype@yandex.ru>
2022-04-05 21:13:43 +0300

    ControlWork has been solved and commited

---
+++ ControlWork/Program.cs
@@ -0,0 +1,18 @@
+﻿// Контрольная работа
+
+string [] wordsGiven = new string[] {"Cat", "Table", "132", "Independence", "Cup"};
+string [] wordsNeeded = new string [5];
+
+for (int i = 0; i < wordsGiven.Length; i++)
+{
+    if(wordsGiven[i].Length <= 3)
+    {
+        {
+            wordsNeeded[i] = wordsGiven[i];
+            Console.WriteLine(wordsNeeded[i]);  
+        }
+    }
+
+}
+
+
